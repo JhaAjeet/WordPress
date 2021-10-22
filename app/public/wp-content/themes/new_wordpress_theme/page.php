25515cf207f93896dd42698edd587de2df2b5354
Ajeet Jha <ajeetjha50@gmail.com>
2021-10-23 00:50:14 +0530

    Parent & Children Pages

--- app/public/wp-content/themes/new_wordpress_theme/page.php
+++ app/public/wp-content/themes/new_wordpress_theme/page.php
@@ -15,11 +15,18 @@ while(have_posts()){
     </div>
 
     <div class="container container--narrow page-section">
+
+      <?php
+      $parentPage = wp_get_post_parent_id(get_the_ID());
+      if($parentPage){ ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentPage) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentPage); ?></a> <span class="metabox__main"><?php echo the_title(); ?></span>
         </p>
-      </div>
+      </div> 
+      <?php }
+
+      ?>
 
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
