b730bef5718bb03888cbd3d3c79fe6ec9b9db97e
Ajeet Jha <ajeetjha50@gmail.com>
2021-10-23 02:38:13 +0530

    Menu of Child Page Links

--- app/public/wp-content/themes/new_wordpress_theme/page.php
+++ app/public/wp-content/themes/new_wordpress_theme/page.php
@@ -28,13 +28,44 @@ while(have_posts()){
 
       ?>
 
+      <?php 
+
+        $testArray = get_pages(array(
+
+        'child_of' => get_the_ID()
+        ));
+        if($parentPage or $testArray) { ?>
+
       <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($parentPage); ?>"><?php echo get_the_title(); ?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <?php 
+
+          if($parentPage){
+            $findChildrenOf = $parentPage;
+          }
+          else{
+            $findChildrenOf = get_the_ID();
+
+          }
+
+          wp_list_pages( array(
+
+            'title_li' => null,
+            'child_of' => $findChildrenOf
+
+          ));
+
+// wp_list_pages(array(
+// 'title_li' => null,
+// 'child_of' => get_the_ID()
+// ));
+
+          ?>
         </ul>
       </div>
+
+    <?php } ?>
 
       <div class="generic-content">
         <?php the_content();  ?>
