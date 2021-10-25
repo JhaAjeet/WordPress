6b65d1b79802381b6eb3276ce2d0b0a3b575410f
Ajeet Jha <ajeetjha50@gmail.com>
2021-10-26 01:39:04 +0530

    Blog: Few Quick Edits & Improvements

--- app/public/wp-content/themes/new_wordpress_theme/front-page.php
+++ app/public/wp-content/themes/new_wordpress_theme/front-page.php
@@ -79,7 +79,7 @@ $homepagePosts->the_post(); ?>
             </div>
           </div>
 
-          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+          <p class="t-center no-margin"><a href="<?php echo site_url('/blog');  ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
       </div>
     </div>
