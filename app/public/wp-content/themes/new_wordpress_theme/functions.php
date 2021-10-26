21930187ba3c2a50b0210d3a719811d27ef66a93
Ajeet Jha <ajeetjha50@gmail.com>
2021-10-26 15:37:42 +0530

    Custom Post Types Event

--- app/public/wp-content/themes/new_wordpress_theme/functions.php
+++ app/public/wp-content/themes/new_wordpress_theme/functions.php
@@ -24,4 +24,18 @@ add_action('after_setup_theme','wordpress_samsung');
 
 
 
+function wordpress_post_types(){
+	register_post_type('event',array(
+		'public' => true,
+		'labels' => array(
+			'name' => 'Events',
+			'add_new_item' => 'Add New Events',
+			'edit_item' => 'Edit Event',
+			'all_items' => 'All Events',
+			'singular_name' => 'Event'
+		),
+		'menu_icon' => 'dashicons-calendar'
+	));
+}
 
+add_action('init','wordpress_post_types');
