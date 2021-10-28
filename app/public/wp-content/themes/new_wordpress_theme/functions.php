bd8f5633987d579261a9b3eff0e71d71c23f91a8
Ajeet Jha <ajeetjha50@gmail.com>
2021-10-28 22:51:16 +0530

    Manipulating Default URL Based Queries in event

--- app/public/wp-content/themes/new_wordpress_theme/functions.php
+++ app/public/wp-content/themes/new_wordpress_theme/functions.php
@@ -44,3 +44,24 @@ function wordpress_post_types(){
 }
 
 add_action('init','wordpress_post_types');
+
+
+function university_adjust_query($query){
+if(! is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
+	$date = date('Ymd');
+	$query->set('meta_key','event_date');
+	$query->set('orderby','meta_value_num');
+	$query->set('order','ASC');
+	$query->set('meta_query',array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $date,
+                'type' => 'Numeric'
+              )
+              
+            ));
+}
+
+}
+add_action('pre_get_posts','university_adjust_query');
